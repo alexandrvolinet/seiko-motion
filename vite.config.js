a19069846af7aeee9fc2c4a71a022c80025bcaa6
alexandrvolinet <alexandr.volinet@gmail.com>
2026-03-06 23:10:21 +0200

    some annimation and adaptive

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/seiko-motion',
+  base: "/seiko-motion/",
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
   },
 });
